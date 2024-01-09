91fd255aab4b190fb994190e84736b01beac292e
nhuandt26 <dothenhuan26@gmail.com>
2024-01-09 23:16:43 +0700

    up

---
+++ B2/DoTheNhuan_2021600381_20233IT6034001/DoTheNhuan_2021600381/Form1.cs
@@ -0,0 +1,10 @@
+namespace DoTheNhuan_2021600381
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ B2/LuocDo/LuocDo/Form1.cs
@@ -0,0 +1,10 @@
+namespace LuocDo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
